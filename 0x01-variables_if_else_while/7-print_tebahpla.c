0150569787923c65409e8b2dae6efd15c665a3b1
Xemb-dev <olayilepraise@gmail.com>
2022-03-15 05:01:13 +0000

    prints the alphabet backwards"

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main-Engry point
+*Inserts the data into n
+*and prints the operator value of n
+*and the last digit of n
+*@: used for holding randum number
+*Return:0
+**/
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
