990bb1fdf93bca21eae43223725946f93757b13a
Xemb-dev <olayilepraise@gmail.com>
2022-03-15 04:44:33 +0000

    prints the alphabet except the letter e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main-Engry point
+*Inserts the data into n
+*and prints the operator value of n
+*and the last digit of n
+*@: used for holding randum number
+*Return:0
+**/
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'e' || letter == 'q')
+		{
+			letter++;
+			continue;
+		}
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
