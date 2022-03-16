0b9356ca40fc8bb5a4abfc73fea21285b1d172d7
Xemb-dev <olayilepraise@gmail.com>
2022-03-16 20:20:38 +0000

    prints the absolute value of any input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+  *_abs - returns the absolute value of an integer
+  *@n: user input
+  *Return: returns the absolute value
+  **/
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
