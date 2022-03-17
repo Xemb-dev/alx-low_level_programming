57c1fecf3d76135e4c0694ce4f3234d327389256
Xemb-dev <olayilepraise@gmail.com>
2022-03-17 19:24:28 +0000

    prints the last digit of the input

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,21 @@
+#include "main.h"
+
 /**
   *print_last_digit - prints the last digit of any number
   *
   *@c: the input parameter
+  *@l: holds the value for the last digit
   *
   *Return: returns the last digit of the input integer
   **/
 int print_last_digit(int c)
 {
-	return (c % 10);
+	int l;
+
+	if (c >= 0)
+		l = c;
+	else
+		l = c * -1;
+	l = l % 10;
+	return (l);
 }
